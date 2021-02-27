09a74e6b059b07c1683a186e51fe00fe0ea20593
Sebastian Marines <sebastian0marines@gmail.com>
2021-02-26 22:39:09 -0600

    Disable docs if running on heroku

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from decouple import config
 
-app = FastAPI()
+
+PRODUCTION = config("PRODUCTION", default=False)
+CONFIG = {
+    'openapi_url': '/openapi.json',
+    'docs_url': '/docs',
+    'redoc_url': '/redoc'
+}
+
+if PRODUCTION:
+    CONFIG = {key: None for key in CONFIG}
+
+app = FastAPI(**CONFIG)
 
 
 @app.get("/")
