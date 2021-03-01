132706c6eb4b51b51d5bbb159c8aed0f3968c4f4
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-01 12:12:55 -0600

    Hash password when creating a new user

--- app/app/crud/crud_user.py
+++ app/app/crud/crud_user.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 
 from app.db import models
 import app.schemas as schemas
+from app.core import passwords
 
 
 def get_user(db: Session, user_id: int):
@@ -17,8 +18,8 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed_password = user.password + "nothashed"  # TODO hash passwords
-    db_user = models.User(email=user.email, hashed_password=fake_hashed_password)
+    hashed_password = passwords.get_password_hash(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
