6ee666ee4eebd5de81c5bcc472b6d46d6720e8d9
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-01 10:29:44 -0600

    Create user creation route

---
+++ app/app/routers/endpoints/user.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from ...dependencies import get_db
+import app.schemas as schemas
+import app.crud as crud
+
+router = APIRouter()
+
+
+@router.post("/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return crud.create_user(db=db, user=user)
