4badccbcb5a926abfc7d78c22769df9d41cbfd55
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-02 10:53:13 -0600

    Create authentication functions

--- app/app/core/security.py
+++ app/app/core/security.py
@@ -1,6 +1,8 @@
 from datetime import timedelta, datetime
 from typing import Optional
 
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from jose import JWTError, jwt
 
@@ -8,6 +10,10 @@ from app.crud import get_user_by_email
 import app.schemas as schemas
 from .passwords import verify_password
 from . import settings
+from app.dependencies import get_db
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/user/get_token")
 
 
 def authenticate_user(db: Session, email: str, password: str) -> Optional[schemas.User]:
@@ -28,3 +34,29 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
+
+
+async def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user_by_email(db, email=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+
+async def get_current_active_user(current_user: schemas.User = Depends(get_current_user)):
+    if not current_user.is_active:
+        raise HTTPException(status_code=400, detail="Email not verified")
+    return current_user
