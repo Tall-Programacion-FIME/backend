8a5edec83ddff8059a2585cfc08c95f75d3302f4
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-02 15:18:03 -0600

    Add JWT config in main file

--- app/app/main.py
+++ app/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from fastapi_jwt_auth import AuthJWT
 
 from .core.settings import settings
 from .routers.api import router
+
+
+@AuthJWT.load_config
+def get_config():
+    return settings
+
 
 app = FastAPI(**settings.docs_config)
 
