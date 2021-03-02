0092704dde0c603087d7c4128cf8fae937c03440
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-02 10:17:38 -0600

    Validate email on account create

--- app/app/routers/endpoints/user.py
+++ app/app/routers/endpoints/user.py
@@ -9,13 +9,15 @@ from ...dependencies import get_db
 import app.schemas as schemas
 import app.crud as crud
 from app.core.security import authenticate_user, create_access_token
-from app.core import settings
+from app.core import settings, utils
 
 router = APIRouter()
 
 
 @router.post("/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    if not utils.is_valid_email_domain(user.email):
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not a valid email domain")
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
