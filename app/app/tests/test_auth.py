dc0d44340eb5bdc22dcc531e0342c723f4f49c77
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-02 13:33:39 -0600

    Test already registered mails

--- app/app/tests/test_auth.py
+++ app/app/tests/test_auth.py
@@ -8,15 +8,9 @@ from ..main import app
 client = TestClient(app)
 
 
-def get_random_string(length: int = 5) -> str:
-    letters = string.ascii_lowercase
-    result_str = ''.join(random.choice(letters) for _ in range(length))
-    return result_str
-
-
 def test_create_user():
     """Tests register function"""
-    email = get_random_string() + "@uanl.edu.mx"
+    email = "testingadmin@uanl.edu.mx"
     password = "admin"
     res = client.post("/user/", json={
         "email": email,
@@ -26,6 +20,22 @@ def test_create_user():
     assert res.status_code == 200
     assert json_response["email"] == email
     assert json_response["is_active"] is True
+
+
+def test_already_registered_email():
+    email = "testingadmin@uanl.edu.mx"
+    password = "admin"
+    res = client.post("/user/", json={
+        "email": email,
+        "password": password
+    })
+    assert res.status_code == 400
+
+
+def test_get_user_by_id():
+    res = client.get("/user/1")
+    assert res.status_code == 200
+    assert "email" in res.text
 
 
 def test_valid_email():
