8d37823f652a9c0457ed506e05c6886f759edac5
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-02 13:52:13 -0600

    Test non existing id

--- app/app/tests/test_auth.py
+++ app/app/tests/test_auth.py
@@ -1,5 +1,4 @@
-import random
-import string
+import uuid
 
 from fastapi.testclient import TestClient
 
@@ -7,11 +6,15 @@ from ..main import app
 
 client = TestClient(app)
 
+email = uuid.uuid4().hex + "testingadmin@uanl.edu.mx"
+password = "admin"
+user_id = None
+access_token = None
+
 
 def test_create_user():
     """Tests register function"""
-    email = "testingadmin@uanl.edu.mx"
-    password = "admin"
+    global user_id
     res = client.post("/user/", json={
         "email": email,
         "password": password
@@ -20,11 +23,10 @@ def test_create_user():
     assert res.status_code == 200
     assert json_response["email"] == email
     assert json_response["is_active"] is True
+    user_id = json_response["id"]
 
 
 def test_already_registered_email():
-    email = "testingadmin@uanl.edu.mx"
-    password = "admin"
     res = client.post("/user/", json={
         "email": email,
         "password": password
@@ -33,9 +35,26 @@ def test_already_registered_email():
 
 
 def test_get_user_by_id():
-    res = client.get("/user/1")
+    res = client.get(f"/user/{user_id}")
     assert res.status_code == 200
     assert "email" in res.text
+
+
+def test_non_existing_id():
+    res = client.get("/user/1000000")
+    assert res.status_code == 404
+
+
+def test_login_for_access_token():
+    global access_token
+    res = client.post("/user/get_token", data={
+        "username": email,
+        "password": password
+    })
+    json_res = res.json()
+    assert res.status_code == 200
+    access_token = json_res["access_token"]
+    assert access_token is not None
 
 
 def test_valid_email():
