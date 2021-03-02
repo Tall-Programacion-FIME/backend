68bf00c5c80c1eaa7ef4fe0e89d31932656cf4c5
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-02 13:21:24 -0600

    Change response code to 400 when an invalid email domain is received

--- app/app/routers/endpoints/user.py
+++ app/app/routers/endpoints/user.py
@@ -18,7 +18,7 @@ router = APIRouter()
 @router.post("/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     if not utils.is_valid_email_domain(user.email):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not a valid email domain")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not a valid email domain")
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email already registered")
