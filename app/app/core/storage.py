57ec9be57c7e024dcce54936bbb213fb32b2f781
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-03 22:03:17 -0600

    Create minio object

---
+++ app/app/core/storage.py
@@ -0,0 +1,12 @@
+from minio import Minio
+
+from .settings import settings
+
+IS_PRODUCTION = True if settings.ENVIRONMENT == "PRODUCTION" else False
+
+client = Minio(
+    endpoint="s3.amazonaws.com" if IS_PRODUCTION else "minio:9000",
+    access_key=settings.AWS_ACCESS_KEY,
+    secret_key=settings.AWS_SECRET_KEY,
+    secure=True if IS_PRODUCTION else False
+)
