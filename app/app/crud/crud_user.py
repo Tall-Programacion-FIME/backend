4f02cbc00412d594580616dda27b15f7c3cd9936
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-03 18:36:25 -0600

    Annotate crud return types

--- app/app/crud/crud_user.py
+++ app/app/crud/crud_user.py
@@ -4,15 +4,15 @@ from app.db import models
 from sqlalchemy.orm import Session
 
 
-def get_user(db: Session, user_id: int):
+def get_user(db: Session, user_id: int) -> schemas.User:
     return db.query(models.User).filter(models.User.id == user_id).first()
 
 
-def get_user_by_email(db: Session, email: str):
+def get_user_by_email(db: Session, email: str) -> schemas.User:
     return db.query(models.User).filter(models.User.email == email).first()
 
 
-def create_user(db: Session, user: schemas.UserCreate):
+def create_user(db: Session, user: schemas.UserCreate) -> schemas.User:
     hashed_password = passwords.get_password_hash(user.password)
     db_user = models.User(email=user.email, name=user.name, hashed_password=hashed_password)
     db.add(db_user)
