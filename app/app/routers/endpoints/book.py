a73c57b2ac551dbf94006c9b81739ee8f6281aba
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-03 22:03:43 -0600

    Store image in S3 bucket

--- app/app/routers/endpoints/book.py
+++ app/app/routers/endpoints/book.py
@@ -1,5 +1,10 @@
+from os import path
+from uuid import uuid4
+
 import app.crud as crud
 import app.schemas as schemas
+from app.core.settings import settings
+from app.core.storage import client as storage_client
 from fastapi import APIRouter, Depends, UploadFile, File, Form
 from fastapi.security import OAuth2PasswordBearer
 from fastapi_jwt_auth import AuthJWT
@@ -11,12 +16,21 @@ router = APIRouter()
 
 
 @router.post("/create_book", response_model=schemas.Book)
-def create_book(name: str = Form(...), author: str = Form(...), cover: UploadFile = File(...),
-                token: str = Depends(OAuth2PasswordBearer(tokenUrl="/token")),
-                authorize: AuthJWT = Depends(), db: Session = Depends(get_db)):
-    # TODO save to S3
+async def create_book(name: str = Form(...), author: str = Form(...), cover: UploadFile = File(...),
+                      token: str = Depends(OAuth2PasswordBearer(tokenUrl="/token")),
+                      authorize: AuthJWT = Depends(), db: Session = Depends(get_db)):
     authorize.jwt_required(token=token)
     current_user = authorize.get_jwt_subject()
     user = crud.get_user_by_email(db, email=current_user)
-    book = schemas.BookCreate(name=name, author=author, cover_url=cover.filename)
+    _, file_extension = path.splitext(cover.filename)
+    new_file_name = uuid4().hex + file_extension
+    stored_image = storage_client.put_object(
+        bucket_name=settings.BUCKET_NAME,
+        object_name=new_file_name,
+        data=cover.file,
+        length=-1,
+        part_size=10 * 1024 * 1024
+    )
+    # TODO append s3 bucket url to cover_url
+    book = schemas.BookCreate(name=name, author=author, cover_url=stored_image.object_name)
     return crud.create_book(db, book=book, user_id=user.id)
