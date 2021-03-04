f69f58f54f2f2f72a01121361147c77c4287c0ca
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-03 22:03:03 -0600

    Create S3 config variables

--- app/app/core/settings.py
+++ app/app/core/settings.py
@@ -12,6 +12,9 @@ class __Settings(BaseSettings):
     authjwt_secret_key: str = Field(..., env="SECRET_KEY")
     ALGORITHM: str = "HS256"
     docs_config: dict = None
+    AWS_ACCESS_KEY: str
+    AWS_SECRET_KEY: str
+    BUCKET_NAME: str = "book-covers"
 
 
 _settings = __Settings()
