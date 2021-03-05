1d7ecb8e1946b20ebd6d51175eecfaab1605d66a
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-05 14:04:08 -0600

    Add custom error messages for jwt exceptions

--- app/app/main.py
+++ app/app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi_jwt_auth import AuthJWT
+from fastapi_jwt_auth.exceptions import AuthJWTException
 
 from .core.settings import settings
 from .routers.api import router
@@ -11,5 +13,14 @@ def get_config():
 
 
 app = FastAPI(**settings.docs_config)
+
+
+@app.exception_handler(AuthJWTException)
+def authjwt_exception_handler(request: Request, exc: AuthJWTException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"detail": exc.message}
+    )
+
 
 app.include_router(router)
