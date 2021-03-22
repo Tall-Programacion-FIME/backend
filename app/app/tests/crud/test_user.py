71bc37f1a66b435c7b2fb69dfaee024da9f59906
Sebastian Marines <sebastian0marines@gmail.com>
2021-03-22 15:40:48 -0600

    Add tests for user authentication and book creation

--- app/app/tests/crud/test_user.py
+++ app/app/tests/crud/test_user.py
@@ -3,13 +3,33 @@ from faker import Faker
 
 from app.tests.utils.utils import random_email, random_lower_string
 from app.schemas.user import UserCreate
+from app.core.security import authenticate_user
 from app.crud.crud_user import create_user
 
 
 def test_create_user(db: Session, fake: Faker) -> None:
     email = random_email()
     password = random_lower_string()
-    user_in = UserCreate(email=email, password=password, name=fake.name())
+    name = fake.name()
+    user_in = UserCreate(email=email, password=password, name=name)
     user = create_user(db, user_in)
     assert user.email == email
     assert hasattr(user, "hashed_password")
+
+
+def test_authenticate_user(db: Session, fake: Faker) -> None:
+    email = random_email()
+    password = random_lower_string()
+    name = fake.name()
+    user_in = UserCreate(email=email, password=password, name=name)
+    user = create_user(db, user_in)
+    authenticated_user = authenticate_user(db, email=email, password=password)
+    assert authenticated_user
+    assert user.email == authenticated_user.email
+
+
+def test_not_authenticate_user(db: Session) -> None:
+    email = random_email()
+    password = random_lower_string()
+    user = authenticate_user(db, email=email, password=password)
+    assert user is None
