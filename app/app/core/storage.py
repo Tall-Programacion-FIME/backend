d65e236f250901e1c1d95080693f85ff2c8750f3
Sebastian Marines <sebastian0marines@gmail.com>
2021-04-24 17:06:17 -0500

    Update S3 endpoint

--- app/app/core/storage.py
+++ app/app/core/storage.py
@@ -9,7 +9,7 @@ s3 = boto3.client(
     "s3",
     aws_access_key_id=settings.AWS_ACCESS_KEY,
     aws_secret_access_key=settings.AWS_SECRET_KEY,
-    endpoint_url="s3.amazonaws.com" if IS_PRODUCTION else "http://minio:9000",
+    endpoint_url="https://s3.amazonaws.com" if IS_PRODUCTION else "http://minio:9000",
     config=Config(signature_version="s3v4"),
     region_name="us-east-1",
 )
