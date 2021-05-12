3d18e859a2e5571f0866175c12c97e197e551804
HectorMtz22 <hec.gre81@gmail.com>
2021-05-11 19:28:00 -0500

    Re-test

--- app/app/tests/crud/test_book.py
+++ app/app/tests/crud/test_book.py
@@ -3,19 +3,18 @@ import random
 from app.crud.crud_book import create_book
 from app.schemas.book import BookCreate
 from app.tests.utils.user import create_random_user
-from elasticsearch import Elasticsearch
 from faker import Faker
 from sqlalchemy.orm import Session
 
 
-def test_create_book(db: Session, es: Elasticsearch, fake: Faker) -> None:
+def test_create_book(db: Session, fake: Faker) -> None:
     name = " ".join(fake.words(3))
     author = fake.name()
     cover_url = fake.image_url()
     price = random.randint(0, 500)
     book_in = BookCreate(name=name, author=author, cover_url=cover_url, price=price)
     user = create_random_user(db=db, fake=fake)
-    book = create_book(db=db, es=es, book=book_in, user_id=user.id)
+    book = create_book(db=db, book=book_in, user_id=user.id)
     assert book.name == name
     assert book.author == author
     assert book.cover_url == cover_url
